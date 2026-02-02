553d323316cc1ef5d1b73077fe42be864e3e096d
Lorenzo Pichilli <pichillilorenzo@gmail.com>
2026-02-02 18:21:52 +0100

    Add Swift Package Manager support for iOS and macOS

---
+++ flutter_inappwebview_ios/ios/flutter_inappwebview_ios/Sources/flutter_inappwebview_ios/InAppBrowser/InAppBrowserNavigationController.swift
@@ -0,0 +1,20 @@
+//
+//  InAppBrowserNavigationController.swift
+//  flutter_inappwebview
+//
+//  Created by Lorenzo Pichilli on 14/02/21.
+//
+
+import Foundation
+import UIKit
+
+public class InAppBrowserNavigationController: UINavigationController {
+    var tmpWindow: UIWindow?
+
+    deinit {
+        debugPrint("InAppBrowserNavigationController - dealloc")
+        tmpWindow?.windowLevel = UIWindow.Level(rawValue: 0.0)
+        tmpWindow = nil
+        UIApplication.shared.delegate?.window??.makeKeyAndVisible()
+    }
+}
